d2e47510b203b3e91adf87fd715bc1872f1bf81b
ah8747980 <ah8747980@gmail.com>
2021-08-02 21:30:04 +0000

    manipulating lists

--- listmethods/listmeth03.py
+++ listmethods/listmeth03.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
->>> fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
 print(fruits)
-Print(proto[
 
->>> fruits.count('apple')
+print(fruits[1])
+
+fruits.append("watermelon")
+print(fruits)
+
+
+
