17e938b63e5d586a49f5c56b36fd71c985deac6d
ah8747980 <ah8747980@gmail.com>
2021-08-03 17:38:31 +0000

    your commit message

--- Jeopardy.py
+++ Jeopardy.py
@@ -3,7 +3,13 @@
 #Welcome Message
 print("WELCOME TO JEOPARDY")
 
+def main():
+
 # print question
 print("What is the Navy's Birthday?")
 # pause and collect input (wait for ENTER)
 quest = input()
+
+
+
+
