1f7450bbdd14d339852f0ec7712daf520f9af63e
ah8747980 <ah8747980@gmail.com>
2021-08-03 16:31:06 +0000

    creating my first game

---
+++ Jeopardy.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+#Welcome Message
+print("WELCOME TO JEOPARDY")
+
+# print question
+print("What is the Navy's Birthday?")
+# pause and collect input (wait for ENTER)
+quest = input()
